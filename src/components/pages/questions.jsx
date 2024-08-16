de2c4ae68744a28ca33a019792537a7b9d483a70
Tonyyang2525 <tonyyang2525@gmail.com>
2024-08-16 13:13:12 -0400

    quiz page
    completed quiz geneartor page(static)

---
+++ src/components/pages/questions.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function questions() {
+  return (
+    <div>
+      <h1>1 of 5</h1>
+      <h3>Question</h3>
+      <p>
+        Can you explain what Javasript is and how it is used to add
+        interactivity to the webiste
+      </p>
+      <h3>Your Answer</h3>
+      <input type="text" />
+
+      <button class="waves-effect waves-light btn">NEXT</button>
+    </div>
+  );
+}
--- src/components/pages/quiz.jsx
+++ src/components/pages/quiz.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
 
-export default function quiz() {
-  return <div></div>;
+export default function Quiz() {
+  return (
+    <div className="quiz-container">
+      <h1>Quiz Generation Options</h1>
+      <h6>Please choose your preferences to generate your personal quiz</h6>
+      <label>Topic</label>
+      <select class="browser-default">
+        <option value=""></option>
+        <option value="1">golang</option>
+        <option value="2">aws</option>
+        <option value="4">javascript</option>
+        <option value="5">CI/CD</option>
+        <option value="6">home gardens</option>
+        <option value="7">coffe</option>
+        <option value="8">finger foods</option>
+      </select>
+      <label>Expertise</label>
+      <select class="browser-default">
+        <option value=""></option>
+        <option value="1">novice</option>
+        <option value="2">intermediate</option>
+        <option value="3">expert</option>
+      </select>
+      <label>Number of questions</label>
+      <select class="browser-default">
+        <option value=""></option>
+        <option value="1">5</option>
+        <option value="2">10</option>
+        <option value="3">15</option>
+      </select>
+      <label>Style of Questions</label>
+      <select class="browser-default">
+        <option value=""></option>
+        <option value="1">master oogway</option>
+        <option value="2">1940's gangster</option>
+        <option value="3">like I'm an 8 year old</option>
+        <option value="3">normal</option>
+        <option value="3">jedi</option>
+        <option value="3">captain jack sparrow</option>
+        <option value="3">matthew mcconaughey</option>
+      </select>
+
+      <button class="waves-effect waves-light btn" onClick={() => {}}>
+        SUBMIT
+      </button>
+    </div>
+  );
 }
