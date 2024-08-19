14c753fd32974fefc74ce445c9c2bb31170ec92a
Tonyyang2525 <tonyyang2525@gmail.com>
2024-08-19 10:36:00 -0400

    quiz page
     questions are recieved now and can generate differently on each one.

--- src/components/pages/quiz.jsx
+++ src/components/pages/quiz.jsx
@@ -1,51 +1,92 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
+import "../css/quiz.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { jsonrepair } from "jsonrepair";
 
 export default function Quiz() {
+  const navigate = useNavigate();
+
+  const generateQuiz = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const topic = formData.get("topic");
+    const expertise = formData.get("expertise");
+    const numberOfQuestions = formData.get("number");
+    const styleOfQuestion = formData.get("style");
+    const data = {
+      topic,
+      expertise,
+      numberOfQuestions,
+      styleOfQuestion,
+    };
+    axios
+      .post("http://localhost:3000/api/createQuiz", data)
+      .then((response) => {
+        const data = response.data.results;
+
+        const trimmedJSON = data.replace(/^Quiz:\s*/, "").trim();
+        const repairedJSON = JSON.parse(jsonrepair(trimmedJSON));
+
+        if (response.data != null)
+          navigate("/questions", { state: repairedJSON });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <div className="quiz-container">
-      <h1>Quiz Generation Options</h1>
-      <h6>Please choose your preferences to generate your personal quiz</h6>
-      <label>Topic</label>
-      <select class="browser-default">
-        <option value=""></option>
-        <option value="1">golang</option>
-        <option value="2">aws</option>
-        <option value="4">javascript</option>
-        <option value="5">CI/CD</option>
-        <option value="6">home gardens</option>
-        <option value="7">coffe</option>
-        <option value="8">finger foods</option>
-      </select>
-      <label>Expertise</label>
-      <select class="browser-default">
-        <option value=""></option>
-        <option value="1">novice</option>
-        <option value="2">intermediate</option>
-        <option value="3">expert</option>
-      </select>
-      <label>Number of questions</label>
-      <select class="browser-default">
-        <option value=""></option>
-        <option value="1">5</option>
-        <option value="2">10</option>
-        <option value="3">15</option>
-      </select>
-      <label>Style of Questions</label>
-      <select class="browser-default">
-        <option value=""></option>
-        <option value="1">master oogway</option>
-        <option value="2">1940's gangster</option>
-        <option value="3">like I'm an 8 year old</option>
-        <option value="3">normal</option>
-        <option value="3">jedi</option>
-        <option value="3">captain jack sparrow</option>
-        <option value="3">matthew mcconaughey</option>
-      </select>
+      <form onSubmit={generateQuiz}>
+        <h1>Quiz Generation Options</h1>
+        <h6>Please choose your preferences to generate your personal quiz</h6>
+        <label>Topic</label>
+        <select className="browser-default" name="topic">
+          <option value=""></option>
+          <option value="golang">golang</option>
+          <option value="aws">aws</option>
+          <option value="javascript">javascript</option>
+          <option value="CI/CD">CI/CD</option>
+          <option value="home gardens">home gardens</option>
+          <option value="coffee">coffee</option>
+          <option value="finger foods">finger foods</option>
+        </select>
+        <label>Expertise</label>
+        <select className="browser-default" name="expertise">
+          <option value=""></option>
+          <option value="novice">novice</option>
+          <option value="intermediate">intermediate</option>
+          <option value="expert">expert</option>
+        </select>
+        <label>Number of questions</label>
+        <select className="browser-default" name="number">
+          <option value=""></option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+        <label>Style of Questions</label>
+        <select className="browser-default" name="style">
+          <option value=""></option>
+          <option value="master oogway">master oogway</option>
+          <option value="1940's gangster">1940's gangster</option>
+          <option value="like I'm an 8 year old">like I'm an 8 year old</option>
+          <option value="normal">normal</option>
+          <option value="jedi">jedi</option>
+          <option value="captain jack sparrow">captain jack sparrow</option>
+          <option value="matthew mcconaughey">matthew mcconaughey</option>
+        </select>
 
-      <button class="waves-effect waves-light btn" onClick={() => {}}>
-        SUBMIT
-      </button>
+        <button
+          className="waves-effect waves-light btn"
+          style={{ maxWidth: "fit-content" }}
+          type="submit"
+        >
+          SUBMIT
+        </button>
+      </form>
     </div>
   );
 }
